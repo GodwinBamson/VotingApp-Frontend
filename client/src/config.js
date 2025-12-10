2de67dd245fa0f593ad167973e6d8d791da3c214
Godwin Bamson <bamsongodwin@gmail.com>
2025-12-10 10:04:48 +0100

    first commit

--- client/src/config.js
+++ client/src/config.js
@@ -10,15 +10,7 @@
 // export const BASE_URL = "http://localhost:5000";
 
 
-// // config.js
-// export const BASE_URL = import.meta.env.PROD
-//   ? "https://votingapp-backend-t10i.onrender.com" // Render production
-//   : "http://localhost:5000/api";                      // Local dev
-
-
-// export const BASE_URL = import.meta.env.PROD
-//   ? "https://votingapp-backend-t10i.onrender.com/api" // production backend
-//   : "http://localhost:5000/api";                      // local dev
+                  
 
 
 
@@ -26,8 +18,5 @@
 
 export const BASE_URL = import.meta.env.PROD
   ? import.meta.env.VITE_API_URL
-  : "http://localhost:5000";
+  : "http://localhost:5000/api";
 
-
-// // config.js
-// export const BASE_URL = import.meta.env.VITE_API_URL;
