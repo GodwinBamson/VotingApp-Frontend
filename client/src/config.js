09b1746b91d70bda9d332b8aa0bd0dc4ee608a2d
Godwin Bamson <bamsongodwin@gmail.com>
2025-12-10 04:09:32 +0100

    new-update5

--- client/src/config.js
+++ client/src/config.js
@@ -22,9 +22,14 @@
 
 
 
+// export const BASE_URL = import.meta.env.PROD
+//   ? import.meta.env.VITE_API_URL
+//   : "http://localhost:5000/api";
+
+
 export const BASE_URL = import.meta.env.PROD
   ? import.meta.env.VITE_API_URL
-  : "http://localhost:5000/api";
+  : "http://localhost:5000";
 
 
 // // config.js
