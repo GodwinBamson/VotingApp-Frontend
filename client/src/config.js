4e82d91930cb930e7d832b16f5463afc8918a89f
Godwin Bamson <bamsongodwin@gmail.com>
2025-12-13 17:38:35 +0100

    update

--- client/src/config.js
+++ client/src/config.js
@@ -1,14 +1,8 @@
-// export const BASE_URL = import.meta.env.PROD
-//   ? import.meta.env.VITE_API_URL
-//   : "http://localhost:5000/api";
+export const BASE_URL = import.meta.env.PROD
+  ? import.meta.env.VITE_API_URL
+  : "http://localhost:5000/api";
 
 
-export const BASE_URL = import.meta.env.VITE_API_URL;
-
-
-if (!BASE_URL) {
-throw new Error("VITE_API_URL is not defined");
-}
 
 
 
